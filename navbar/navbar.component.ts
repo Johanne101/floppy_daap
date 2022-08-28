ba1a7ecdc9fd9b51143b5e68ddf5a56d9be7596f
vengator <42843150+vengator@users.noreply.github.com>
2022-08-28 14:09:47 -0400

    Add NavBar
    Neftify Connect SDK is here

---
+++ navbar/navbar.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
+import { ethers } from 'ethers';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+    public isCollapsed = true;
+    private lastPoppedUrl: string;
+    private yScrollStack: number[] = [];
+
+    constructor(public location: Location, private router: Router) { }
+
+    neftifyConnect: any = document.getElementById("neftifyConnect");
+
+    ngOnInit() {
+      this.router.events.subscribe((event) => {
+        this.isCollapsed = true;
+        if (event instanceof NavigationStart) {
+           if (event.url != this.lastPoppedUrl)
+               this.yScrollStack.push(window.scrollY);
+       } else if (event instanceof NavigationEnd) {
+           if (event.url == this.lastPoppedUrl) {
+               this.lastPoppedUrl = undefined;
+               window.scrollTo(0, this.yScrollStack.pop());
+           } else
+               window.scrollTo(0, 0);
+       }
+     });
+     this.location.subscribe((ev:PopStateEvent) => {
+         this.lastPoppedUrl = ev.url;
+     });
+    }
+
+    async getConnectionDetails() {
+        console.log(await this.neftifyConnect.getConnectionDetails());
+        const abi = require('../../../abi.json');
+        let contract = new ethers.Contract("0x60D65232818A6e6A7839ceF93b0A301886D1E51A", abi, await this.neftifyConnect.getSigner());
+
+        let x = await contract['retrieve']();
+        //await contract['store'](8);
+        console.log(parseInt(x._hex, 16));
+    }
+
+    isHome() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+
+        if( titlee === '#/home' ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    isDocumentation() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        if( titlee === '#/documentation' ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
